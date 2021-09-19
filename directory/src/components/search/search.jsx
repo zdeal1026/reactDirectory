6cee08eda3aa48d10c8595e9b88ca3cb2d83ebef
Zdeal <jedimastershake@gmail.com>
2021-09-19 10:01:12 -0400

    added style and search bar

--- directory/src/components/search/search.jsx
+++ directory/src/components/search/search.jsx
@@ -1,5 +1,23 @@
-//import React, { Component } from "react";
+import React from "react";
+import "./style.scss";
 
-export default function Search() {
-  return <div></div>;
-}
+const Search = (props) => {
+  // Setting the component's initial state
+
+  return (
+    <nav className="navbar navbar-light bg-light justify-content-center">
+      <form className="form-inline m-2" onSubmit={props.handleFormSubmit}>
+        <input
+          className="form-control"
+          value={props.value}
+          name="search"
+          onChange={props.handleInputChange}
+          type="search"
+          placeholder="Search"
+        />
+      </form>
+    </nav>
+  );
+};
+
+export default Search;
